86085db5cef6bd59fb317ef7c7ae01eef620419a
code1218 <skjil1218@gmail.com>
2024-06-19 13:59:55 +0900

    react start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+  컴포넌트 -> 함수 -> HTML 태그를 리턴하는 함수
+  JSX
+    1. 태그를 열었으면 닫아야한다. <a></a>, <a />
+    2. 여러 태그를 리턴해야하는 경우에는 하나로 묶어야 한다.
+    3. JSX 안에 값 또는 변수를 사용하려면 {}표현식을 사용해야한다.
+
+*/
+import "./App.css";
+import Box from "./components/Box";
+import CustomInput from "./components/CustomInput";
+import Hello from "./components/Hello";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name = "김준일";
+  const fontColorRed = {
+    color: "red"
+  };
+  // console.log();
+
+  const age = <h2>{31}</h2>;
+
+  return <>
+      <div>
+        <Hello></Hello>
+      </div>
+      {/*<div>
+        <Hello></Hello>
+      </div>*/}
+      <h1 style={fontColorRed} className={"fs-20 itelic"}>{name}</h1>
+      <CustomInput ph={"이름"} disabled={true} value={"김준일"}/>
+      <CustomInput ph={"나이"} disabled={false} />
+      <CustomInput ph={"연락처"} disabled={true} />
+      <Box name={"김준일"} isShow={false} >
+        <h2>{31}</h2>
+        <h2>{31}</h2>
+        <h2>{31}</h2>
+      </Box>
+  </>
 }
 
 export default App;
