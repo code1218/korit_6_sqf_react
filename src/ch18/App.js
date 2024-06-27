56513428a25173b1ef646f94873e8cd9f7200d87
code1218 <skjil1218@gmail.com>
2024-06-27 10:57:15 +0900

    router

--- src/ch18/App.js
+++ src/ch18/App.js
@@ -4,6 +4,7 @@ import MainSidebar from './components/MainSidebar/MainSidebar';
 import MainHeader from './components/MainHeader/MainHeader';
 import { Global } from '@emotion/react';
 import { reset } from './styles/global';
+import MainBody from './components/MainBody/MainBody';
 
 function App(props) {
     const [ isMainSidebarShow, setMainSidebarShow ] = useState(false);
@@ -13,7 +14,7 @@ function App(props) {
             <Global styles={reset}/>
             <MainLayout>
                 <MainHeader setMainSidebarShow={setMainSidebarShow} />
-
+                <MainBody />
                 <MainSidebar 
                     isMainSidebarShow={isMainSidebarShow}
                     setMainSidebarShow={setMainSidebarShow}
